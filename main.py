281abe4e3794bcc894593bd0a249ee67ba25c559
Zoe Murphy <zmurphy@tulane.edu>
2025-02-17 23:27:06 -0600

    final code push

--- main.py
+++ main.py
@@ -6,11 +6,35 @@ See assignment-01.pdf for details.
 
 def foo(x):
     ### TODO
-    pass
+    # Base Case if x is less than or equal to 1
+    if x <= 1:
+        return x
+    else:
+        ra = (foo(x-1)) # defines ra to be one number less than x, also a recursive call
+        rb = (foo(x-2)) # defines rb to be two numbers less than x, also a recursive call
+        return ra + rb # adds ra and rb together
 
 def longest_run(mylist, key):
     ### TODO
-    pass
+    longest = 0 # Will store the longest run of "key"
+    current = 0 # Will track the current run of consecutive key values
+
+    # For loop to interate through the whole list
+    for integer in mylist:
+        # checks if the list value is equal to the key
+        if integer == key:
+            # if list value is equal to key, add one to current
+            current += 1
+        else:
+            # If there is a different number than the key, update the longest value
+            longest = max(longest, current)
+            # Reset the count
+            current = 0
+    # Making sure longest equals the longest run of the key
+    longest = max(longest, current)
+    
+    return longest 
+
 
 
 class Result:
@@ -38,7 +62,31 @@ def to_value(v):
         
 def longest_run_recursive(mylist, key):
     ### TODO
-    pass
+    # Base case if there's only one element in the key
+    if len(mylist) == 1:
+        if mylist[0] == key:
+            # if the one element matches the key, return the corresponsing Result class object
+            return Result(1, 1, 1, True)
+        else: 
+            # if the one element does not match the key, return the corresponsing Result class object
+            return Result(0, 0, 0, False)
+    
+    # Divide and conquer part!
+    middle = len(mylist) // 2 # Split list into two
+    left_result = longest_run_recursive(mylist[:middle], key) # Recursively call the function on the left split of the list
+    right_result = longest_run_recursive(mylist[middle:], key) # Recursively call the function on the right split of the list
+
+    # Handles the left and right run size by checking if it's the entire range of values and if not, it merges them together
+    left_size = left_result.left_size if not left_result.is_entire_range else left_result.left_size + right_result.left_size
+    right_size = right_result.right_size if not right_result.is_entire_range else right_result.right_size + left_result.right_size
+
+    # Computes the longest run size by taking the max of the right recursive result, left recursive result, and the combined ones over the middle
+    longest_size = max(to_value(left_result), to_value(right_result), left_result.right_size + right_result.left_size)
+
+    is_entire_range = left_result.is_entire_range and right_result.is_entire_range
+
+    # Returns result object
+    return Result(left_size, right_size, longest_size, is_entire_range)
 
 
 
